897ab4841c7f8b82af391131175c79f4830e76e7
pinhobrunodev <iaishowcs@gmail.com>
2021-12-25 10:46:44 -0300

    UserController (ById,FindAll,Delete) Service and ServiceImpl

---
+++ authuser/src/main/java/com/ead/authuser/controllers/UserController.java
@@ -0,0 +1,50 @@
+package com.ead.authuser.controllers;
+
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @Autowired
+    private UserServiceImpl userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserModel>> getAllUsers() {
+        return ResponseEntity.ok().body(userService.findAll());
+    }
+
+    @GetMapping(value = "/{userId}")
+    public ResponseEntity<Object> getOneUser(@PathVariable UUID userId) {
+        Optional<UserModel> userModelOptional = userService.findById(userId);
+        if (!userModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        } else {
+            return ResponseEntity.ok().body(userModelOptional.get());
+
+        }
+    }
+
+    @DeleteMapping(value = "/{userId}")
+    public ResponseEntity<Object> deleteUser(@PathVariable UUID userId) {
+        Optional<UserModel> userModelOptional = userService.findById(userId);
+        if (!userModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        } else {
+            userService.delete(userModelOptional.get());
+            return ResponseEntity.ok().body("User deleted successful");
+        }
+
+    }
+
+}
--- authuser/src/main/java/com/ead/authuser/services/UserService.java
+++ authuser/src/main/java/com/ead/authuser/services/UserService.java
@@ -1,6 +1,17 @@
 package com.ead.authuser.services;
 
 
+import com.ead.authuser.models.UserModel;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface UserService {
 
+    List<UserModel> findAll();
+
+    Optional<UserModel> findById(UUID userId);
+
+    void delete(UserModel userModel);
 }
